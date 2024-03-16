fabae05c78c179f351cc83cbb8405c43cd997b44
grace-coding <sanwind0305@gmail.com>
2024-03-16 18:47:32 +0200

    Submit React Exercise

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import ListGroup from "./components/ListGroup"
+
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+
 function App() {
+	const [alertVisible, setAlertVisibility] = useState(false)
 	return (
 		<div>
-			<ListGroup/>
+			{alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>}
+			<Button onClick={() => setAlertVisibility(true)}>My Botton</Button>
 		</div>
 	)
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,20 +1,32 @@
-function ListGroup() {
-  let items = ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix']
-  items = []
-  if (items.length === 0)
-    return (
-      <>
-        <h1>List</h1>
-        <p>No items</p>
-      </>
-    )
+import { useState } from "react"
 
+//{items: [],  heading: String}
+interface Props {
+	items: String[]
+	heading: String
+	onSelectItem: (item: String) => void
+}
+
+
+function ListGroup({items, heading, onSelectItem}: Props) {
+  
+	// hook
+	const [selelctedIndex, setSelectedIndex] = useState(-1)
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
-        {items.map(item => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li className={ selelctedIndex === index ? "list-group-item active" : "list-group-item"} 
+					key={index} 
+					onClick={() => {
+						setSelectedIndex(index)
+						onSelectItem(item)
+					}}
+					>
+            {item}
+          </li>
         ))}
       </ul>
     </>
